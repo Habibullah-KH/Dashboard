82da0f46dcbdff0eea9be2eb496c1ba0047843f4
habibullah <habibullahkhandokar01@gmail.com>
2025-09-04 08:12:56 +0600

    upload image visible

--- src/app/components/PortfolioForm/PortfolioMediaUploader/ImageUpload.tsx
+++ src/app/components/PortfolioForm/PortfolioMediaUploader/ImageUpload.tsx
@@ -17,23 +17,36 @@ export default function ImageUpload({imageData} : ImageUploadProps) {
 console.log(previewUrl);
   return (
     <>
-    <div>
-        <div> {/*image text info*/}
-            <h2>image name</h2>
-            <p>size: 20</p>
-            </div> {/*image text info*/}
-        
-        <div> {/*image | image view*/}
-            <div>
+    <div className={` mt-5 ${imageData ? "block" : "hidden"} `}>
+<div className='flex flex-col-reverse items-center'>
+<div> {/*image text info*/}
+
+<h2>{imageData?.name}</h2>
+<p>{imageData ? (imageData?.size / 1024).toFixed(2) : ""} </p>
+</div> {/*image text info*/}
+    
+<div> {/*image | image view*/}
+<div>
 <Image 
   src={previewUrl ?? ""} 
   alt={imageData?.name || "Uploaded image"} 
   width={200} 
   height={200} 
 />
+</div>
+</div> {/*image | image view*/}
+</div>
 
-            </div>
-            </div> {/*image | image view*/}
+<div className='mt-5'>
+  <button>
+    Cancel
+  </button>
+
+  <button>
+    Upload
+  </button>
+</div>
+
     </div>
     </>
   )
