22fcb9ef42950b31b41f19c79952038b22609740
habibullah <habibullahkhandokar01@gmail.com>
2025-10-01 11:46:41 +0600

    get method added

--- src/app/api/portfolioData/route.ts
+++ src/app/api/portfolioData/route.ts
@@ -1,10 +1,29 @@
 import dbConnect, { collectionNameOpj } from "@/app/lib/dbConnect";
 import { NextResponse } from "next/server"
 
-export const POST = async (req) => {
+// GET data form db
+export const GET = async () => {
+    try{
+        const portfolioCollection = await dbConnect(collectionNameOpj.portfolioData)
+        const data = await portfolioCollection.find({}).toArray();
+        return NextResponse.json(data, {status: 200})
+    }
+    catch(error){
+        console.error('faild get data from database', error)
+        return NextResponse.json({error: 'faild to get'}, {status: 500})
+    }
+}
 
+// POST 'upload' data on db
+export const POST = async (req: Request) => {
+try{
     const body = await req.json(); 
     const portfolioCollection = dbConnect(collectionNameOpj.portfolioData)
     const result = await portfolioCollection.insertOne(body);
     return NextResponse.json(result);
+}
+catch(error){
+console.error('faild to post', error)
+return NextResponse.json({error: 'faild to post'}, {status: 500})
+}
 }
